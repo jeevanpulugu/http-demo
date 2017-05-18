18917ea2f9fb72d9a3188527c34044180c2db28d
Jeevan Pulugu <Jeevan Pulugu>
2017-05-18 15:19:49 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  people: Array<any>;
+  next: string;
+  errorMessage: string = '';
+
+  constructor (public http: Http) {
+    http.get('http://swapi.co/api/people')
+    .map( res => res.json())
+    .subscribe(
+      data => {
+        this.people = data.results;
+        this.next = data.next;
+      },
+      error => {
+        this.errorMessage = error.json()
+        console.log(this.errorMessage);
+      }
+    );
+  }
+
+  gerMorePeople () {
+    this.http.get(this.next)
+    .map(res => res.json())
+    .subscribe(
+      data => {
+        this.people.push(...data.results);
+         this.next = data.next;
+      }
+    );
+
+  }
 }
